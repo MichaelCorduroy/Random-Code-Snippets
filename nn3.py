5228c9f37996d224036ff9c191bab0d5e4cbaeb7
FroggoBoi <33583002+FroggoBoi@users.noreply.github.com>
2019-09-02 14:18:01 -0400

    Create nn3.py

---
+++ nn3.py
@@ -0,0 +1,54 @@
+from numpy import exp, array, random, dot
+
+class NeuralNetwork():
+    def __init__(self):
+        #seed random number (same number every runtimde)
+        random.seed(1)
+        #single neuron; 3 inputs; 1 output
+        self.synaptic_weights = 2 * random.random((3,1)) - 1
+
+    def __sigmoid__(self, x):
+        return 1/(1 + exp(-x))
+
+    #gradient of the sigmoid curve
+    def __sigmoid_derivative(self, x):
+        return x * (1-x)
+
+    def train(self, training_set_inputs, training_set_outputs, number_of_training_iterations):
+        for iteration in range(number_of_training_iterations):
+            #pass training set through neural net
+            output = self.predict(training_set_inputs)
+            #calculate error
+            error = training_set_outputs - output
+
+            adjustment = dot(training_set_inputs.T, error * self.__sigmoid_derivative(output))
+
+            self.synaptic_weights += adjustment
+        
+
+    def predict(self, inputs):
+        return self.__sigmoid__(dot(inputs, self.synaptic_weights))
+
+
+neural_network = NeuralNetwork()
+
+print('Random starting synaptic weights')
+print(neural_network.synaptic_weights)
+
+training_set_inputs = array([[0,0,1],[1,1,1],[1,0,1], [0,1,1]])
+training_set_outputs = array([[0,1,1,0]]).T
+
+neural_network.train(training_set_inputs, training_set_outputs, 10000)
+print("New synaptic weights after training: ")
+print(neural_network.synaptic_weights)
+
+print('predicting: ')
+print( neural_network.predict(array([0, 1, 1])))
+
+while(True):
+    haa = []
+    for i in range(3):
+        v = int(input("Whats the value? "))
+        haa.append(v)
+    
+    print(neural_network.predict(array([haa])))
